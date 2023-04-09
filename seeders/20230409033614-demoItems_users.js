b42f7c1feddc06e9b7458ff52b1986c5ec24e8a0
Nguyễn Đức Huy <nguyenduchuy564@gmail.com>
2023-04-09 11:54:08 +0700

    fix items_users seeder

--- seeders/20230409033614-demoItems_users.js
+++ seeders/20230409033614-demoItems_users.js
@@ -1,20 +1,19 @@
 'use strict';
 const {users, items, products} = require("../models");
 
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
    await queryInterface.bulkInsert('items_users', [{
-     users_id: await users.findOne({where: {email: "huyhwe1@fakemail.com"}}).id,
-     items_id: await items.findOne({where: {size: "M"}}).id,
+     users_id: (await users.findOne({where: {email: "huyhwe1@fakemail.com"}})).id,
+     items_id: (await items.findOne({where: {size: "M"}})).id,
      quantity: 1,
      createdAt: new Date(),
      updatedAt: new Date(),
    },
    {
-    users_id: await users.findOne({where: {email: "huyhwe1@fakemail.com"}}).id,
-    items_id: await items.findOne({where: {size: "L"}}).id,
+    users_id: (await users.findOne({where: {email: "huyhwe1@fakemail.com"}})).id,
+    items_id: (await items.findOne({where: {size: "L"}})).id,
     quantity: 1,
     createdAt: new Date(),
     updatedAt: new Date(),
